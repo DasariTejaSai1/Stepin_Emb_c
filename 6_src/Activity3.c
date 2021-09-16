4ed33cb05845b9b98b8224935d82c8c3e0c80438
DasariTejaSai1 <89805562+DasariTejaSai1@users.noreply.github.com>
2021-09-16 23:45:39 +0530

    Update Activity3.c

--- 6_src/Activity3.c
+++ 6_src/Activity3.c
@@ -5,7 +5,7 @@ void InitTimer()
 {
     TCCR1A |= (1<<COM1A1)|(1<<WGM11)|(1<<WGM10);
     TCCR1B |= (1<<WGM12)|(1<<CS11)|(1<<CS10);
-    DDRB |=(1<<PB1);
+    DDRB |=(1<<PB2);
 }
 
 void Activity3_PWM(uint16_t temp)
